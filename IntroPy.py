fcd24dfc5e4385229a527e2061c7ad5214f92301
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2025-02-27 12:34:34 +0600

    new stuff

---
+++ IntroPy.py
@@ -0,0 +1,28 @@
+# x = 10
+# print(x)
+
+# a = input("What's your name?")
+# print("Your name is"+a)
+
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+# sum = x + y
+# print("Sum of the number: ", sum)
+
+s = "artificial intelligence"
+print(s[:4])
+print(s[-4:])
+
+s2 = "   Python is fun?"
+print(s2[2:9:2])
+
+print(s2.split(' '))
+print(s2.strip())
+
+s3 = "Hello";
+print(s3[0::2])
+
+s4 = input("Enter your string: ")
+
+print(s4[0]+s4[int(len(s4)/2)]+s4[-1])
+
---
+++ Task1.py
@@ -0,0 +1,10 @@
+#a
+s1 = "password1234"
+NewS1 = s1[0].upper() + s1[1:]
+
+print(NewS1)
+
+#c
+s4 = "password1234"
+
+print(s4.replace('s', '$').replace('S', '$'))
