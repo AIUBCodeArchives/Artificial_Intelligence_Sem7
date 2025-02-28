a62fc6d52355ac8269c34e971ede2bd7f11702ad
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2025-02-28 14:43:06 +0600

    b has issues

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Artificial_Intelligence_Sem7
+
+## All lab task codes (python3) of the ARTIFICIAL INTELLIGENCE AND EXPERT SYSTEM course.
--- Task1.py
+++ Task1.py
@@ -1,10 +1,17 @@
-#a
 s = "password1234"
-NewS1 = s[0].upper() + s[1:]
 
+#a
+NewS1 = s[0].upper() + s[1:]
 print(NewS1)
 
-#c
-s = "password1234"
+#b
+print(s.replace('a', 'e').replace('e', 'i').replace('i', 'o').replace('o', 'u').replace('u', 'a'))
 
+#c
 print(s.replace('s', '$').replace('S', '$'))
+
+#d
+Converted_s = s[:-4] + str(int(s[-4:])*2)
+print(Converted_s)
+
+
